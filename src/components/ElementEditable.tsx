bdf4cece65bb27a3cae25da5a9f73c4a1f507d1c
ipiyou <tao710803@gmail.com>
2022-12-04 03:11:05 +0900

    placeholder를 위한 파일

---
+++ src/components/ElementEditable.tsx
@@ -0,0 +1,26 @@
+import { createElement, RefObject, KeyboardEvent, FocusEvent } from "react";
+import Element, { ElementChangeType, ElementType, RefType } from "./Element";
+import "./placeholder.css";
+
+export interface ElementEditableType {
+  element: ElementType;
+  Ref?: RefObject<RefType>;
+  contentEditable?: boolean;
+  value: string;
+  className?: string;
+  style?: Object;
+  onChange: (e: ElementChangeType) => void;
+  onKeyDown?: (e: KeyboardEvent<ElementType>) => void;
+  onKeyPress?: (e: KeyboardEvent<ElementType>) => void;
+  onKeyUp?: (e: KeyboardEvent<ElementType>) => void;
+  onFocus?: (e: FocusEvent<ElementType>) => void;
+  onBlur?: (e: FocusEvent<ElementType>) => void;
+  placeholder?: string;
+}
+
+function ElementEditable(props: ElementEditableType) {
+  const className = "ElementEditable";
+  return createElement("div", { className }, <Element {...props} />);
+}
+
+export default ElementEditable;
